e016eca7bd521123da6c59a8dc69cf6b95b574a5
Fatih Kadir Akın <f@users.noreply.github.com>
2015-11-24 14:01:16 +0200

    Update kiosk.js

--- kiosk.js
+++ kiosk.js
@@ -1,7 +1,7 @@
 function NewsWidget(config) {
   console.log(config);
   var element = document.createElement("div");
-  element.innerHTML = "merhaba";
+  element.innerHTML = "merhaba :) " + config.show_items + " haber gosteriliyor";
   return element;
 }
 
